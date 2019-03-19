0db476eb38fa1eafdd3240efab33d3bfde3b93f4
Cesar Ivan Mtz <bmwcesarim@gmail.com>
2019-03-19 12:36:13 -0600

    Comenzando SAT

---
+++ src/algoritmosgeneticos/MainSAT.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosgeneticos;
+
+import nsat.Individuo;
+import nsat.Tokenizador;
+
+/**
+ *
+ * @author CESAR IVAN MTZ
+ */
+public class MainSAT {
+    public static void main(String args[]){
+        Tokenizador.leerDatos();
+        
+        System.out.println("");
+        Individuo i = new Individuo();
+        Individuo i2 = new Individuo();
+        Individuo i3 = new Individuo();
+        
+        System.out.println();    
+    }
+}
